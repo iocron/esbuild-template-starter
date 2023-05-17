58fdcd973cd04d094c845d71f72e1b1aefc5ecca
iocron <info@iocron.com>
2023-05-17 04:07:56 +0200

    Added exports and simple modular arg options

--- bundle.mjs
+++ bundle.mjs
@@ -1,6 +1,9 @@
 import * as esbuild from 'esbuild'
 
-let ctx = await esbuild.context({
+const args = process.argv.slice(2)
+const buildType = args[0]
+
+export let _esOptions = {
     outdir: 'dist',
     entryPoints: [
         { out: 'bundle', in: './src/main.js'},
@@ -12,7 +15,37 @@ let ctx = await esbuild.context({
     sourcemap: true,
     // packages: 'external', // External dependency loading during runtime
     // target: ['node10.4'], // Specify target (node version) if needed
-})
+}
 
-await ctx.watch()
-console.log('watching...')
+export const _esBuilder = async function(){
+    await esbuild.build(_esOptions)
+    console.log('Build seems complete.')
+}
+
+export const _esWatcher = async function(){
+    const ctx = await esbuild.context(_esOptions)
+    await ctx.watch()
+    console.log('Watching...')
+}
+
+export default {
+    _esOptions,
+    _esBuilder,
+    _esWatcher
+}
+
+switch (buildType) {
+    case "options":
+        console.log("_esOptions: ")
+        console.log(_esOptions)
+        break;
+    case "build":
+        _esBuilder(_esOptions)
+        break;
+    case "watch":
+        _esWatcher(_esOptions)
+        break;
+    default:
+        console.log("Please specify one of the following arguments as string: \"build\", \"watch\" or \"options\"")
+        break;
+}
